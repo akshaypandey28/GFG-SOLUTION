b8c92d0d9b34d4e6e06c08993a878692b5637b30
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-10-02 21:43:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Print Longest Increasing Subsequence/print-longest-increasing-subsequence.cpp
+++ Difficulty: Medium/Print Longest Increasing Subsequence/print-longest-increasing-subsequence.cpp
@@ -4,61 +4,45 @@ using namespace std;
 
 
 // } Driver Code Ends
-
-
 class Solution {
-public:
-    vector<int> dp;
-    vector<int> arr;
-    vector<int> parent; // To store the previous index in the LIS
-
-    // Recursive function to find the length of LIS ending at index i
-    int f(int i) {
-        if (dp[i] != -1) return dp[i];
-        int maxi = INT_MIN;
-        parent[i] = -1; // No previous index initially
-
-        for (int j = 0; j <= i - 1; j++) {
-            if (arr[j] < arr[i]) {
-                int possibleLIS = f(j);
-                if (possibleLIS > maxi) {
-                    maxi = possibleLIS;
-                    parent[i] = j; // Track the previous index
+  public:
+    vector<int> longestIncreasingSubsequence(int n, vector<int>& arr) {
+        vector<int> dp(n,1);
+        vector<int> hash(n,1);
+    
+        for(int i=0; i<=n-1; i++){
+            hash[i] = i;
+            for(int prev_index = 0; prev_index <=i-1; prev_index ++){
+                if(arr[prev_index]<arr[i] && 1 + dp[prev_index] > dp[i]){
+                    dp[i] = 1 + dp[prev_index];
+                    hash[i] = prev_index;
                 }
             }
         }
+    
+        int ans = -1;
+        int lastIndex =-1;
         
-        if (maxi == INT_MIN) return dp[i] = 1; // Base case: LIS length is 1
-        return dp[i] = 1 + maxi; // Add 1 to include arr[i]
-    }
-
-    vector<int> longestIncreasingSubsequence(int n, vector<int>& nums) {
-        arr = nums;
-        dp.clear();
-        parent.clear();
-        dp.resize(n, -1);
-        parent.resize(n, -1);
-
-        int maxLength = 1, lastIndex = 0; // Track the LIS length and last element's index
-        for (int i = 0; i < n; i++) {
-            int length = f(i);
-            if (length > maxLength) {
-                maxLength = length;
-                lastIndex = i; // Update the index of the last element of LIS
+        for(int i=0; i<=n-1; i++){
+            if(dp[i]> ans){
+                ans = dp[i];
+                lastIndex = i;
             }
         }
-
-        // Reconstruct the LIS by backtracking using the parent array
-        vector<int> lis;
-        for (int i = lastIndex; i != -1; i = parent[i]) {
-            lis.push_back(arr[i]);
+        
+        vector<int> temp;
+        temp.push_back(arr[lastIndex]);
+        
+        while(hash[lastIndex] != lastIndex){ // till not reach the initialization value
+            lastIndex = hash[lastIndex];
+            temp.push_back(arr[lastIndex]);    
         }
-
-        reverse(lis.begin(), lis.end()); // Since we backtrack, reverse to get the correct order
-        return lis; // Return the actual subsequence
+        
+        // reverse the array 
+        reverse(temp.begin(),temp.end());
+        return temp;
     }
 };
-
 
 //{ Driver Code Starts.
 
